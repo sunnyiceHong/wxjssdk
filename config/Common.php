3032091f2a655dce894adcf42aa38f7eacd1cdde
Sunny <13631310872@163.com>
2019-08-22 01:57:15 +0800

    1、修改了储存不了内容bug 2、优化了公共方法

---
+++ config/Common.php
@@ -0,0 +1,101 @@
+<?php
+namespace wechat\config;
+
+class Common{
+    /**生成指定长度的随机字符串
+     * @param $ length 指定字符串长度
+     * @return null|string
+     */
+    static function createNonceStr($length = 16) {
+        $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        $str = "";
+        for ($i = 0; $i < $length; $i++) {
+            $str .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
+        }
+        return $str;
+    }
+
+    /**最全的模拟请求方法
+     * @param string $url 请求地址
+     * @param string $type请求方式
+     * @param string $data请求数据
+     * @param bool $header头部数据
+     * @return mixed
+     */
+    static function postCurl($url = '', $type = "POST", $data = '', $header = false)
+    {
+        #1.创建一个curl资源
+        $ch = curl_init();
+        #2.设置URL和相应的选项
+        //2.1设置url
+        curl_setopt($ch, CURLOPT_URL, $url);
+        //2.2设置头部信息
+        //array_push($header, 'Accept:application/json');
+        //array_push($header,'Content-Type:application/json'); //array_push($header, 'http:multipart/form-data'); //设置为false,只会获得响应的正文(true的话会连响应头一并获取到)
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        //curl_setopt ( $ch, CURLOPT_TIMEOUT,5); // 设置超时限制防止死循环
+        //设置发起连接前的等待时间，如果设置为0，则无限等待。
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        //将curl_exec()获取的信息以文件流的形式返回，而不是直接输出。
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        #3设置请求参数
+        if ($data)
+        {
+            //全部数据使用HTTP协议中的"POST"操作来发送。
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        }
+        //3)设置提交方式
+        switch($type){
+            case "GET":
+                curl_setopt($ch,CURLOPT_HTTPGET,true);
+                break;
+            case "POST":
+                curl_setopt($ch,CURLOPT_POST,true);
+                break;
+            case "PUT"://使用一个自定义的请求信息来代替"GET"或"HEAD"作为HTTP请求。这对于执行"DELETE" 或者其他更隐蔽的HTT
+                curl_setopt($ch,CURLOPT_CUSTOMREQUEST,"PUT");
+                break;
+            case "DELETE":
+                curl_setopt($ch,CURLOPT_CUSTOMREQUEST,"DELETE");
+                break;
+        }
+        //4.设置请求头 如果有才设置
+        if ($header)
+        {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        }
+        #5.上传文件相关设置
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
+        // 对认证证书来源的检查
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        // 从证书中检查SSL加密算
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+        #6.在HTTP请求中包含一个"User-Agent: "头的字符串。-----必设
+        //curl_setopt($ch, CURLOPT_USERAGENT, 'SSTS Browser/1.0');
+        //curl_setopt($ch, CURLOPT_ENCODING, 'gzip'); //6.2=1模拟用户使用的浏览器
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)');
+        #7.抓取URL并把它传递给浏览器
+        $result = curl_exec($ch);
+        #8关闭curl资源，并且释放系统资源
+        curl_close($ch);
+        return $result;
+    }//fun
+
+    function httpGet($url) {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 500);
+        // 为保证第三方服务器与微信服务器之间数据传输的安全性，所有微信接口采用https方式调用，必须使用下面2行代码打开ssl安全校验。
+        // 如果在部署过程中代码在此处验证失败，请到 http://curl.haxx.se/ca/cacert.pem 下载新的证书判别文件。
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_URL, $url);
+
+        $res = curl_exec($curl);
+
+        curl_close($curl);
+
+        return $res;
+    }
+}
--- src/auth/WxConfig.php
+++ src/auth/WxConfig.php
@@ -8,93 +8,96 @@
 
 namespace wechat\auth;
 
+use think\Cache;
 use wechat\config\WechatConfig;
+use wechat\config\Common;
 
 class WxConfig {
-  private $appId;
-  private $appSecret;
+    private $appId;
+    private $appSecret;
 
-  public function __construct($appId, $appSecret) {
-      $this->appid = WechatConfig::APPID;
-      $this->appSecret = WechatConfig::APPSECRET;
-  }
-  
-  public function getSignPackage($url = NULL) {
-    $jsapiTicket = $this->getJsApiTicket();
-
-    if(!$url){
-          $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-          $url = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    public function __construct($appId, $appSecret) {
+        $this->appid = WechatConfig::APPID;
+        $this->appSecret = WechatConfig::APPSECRET;
     }
 
-    $timestamp = time();
-    $nonceStr = createNonceStr();
+    public function getSignPackage($url = NULL) {
+        $jsapiTicket = $this->getJsApiTicket();
 
-    // 这里参数的顺序要按照 key 值 ASCII 码升序排序
-    $string = "jsapi_ticket=$jsapiTicket&noncestr=$nonceStr&timestamp=$timestamp&url=$url";
+        if(!$url){
+            $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+            $url = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        }
 
-    $signature = sha1($string);
-    $signPackage = array(
-      "appId"     => $this->appId,
-      "nonceStr"  => $nonceStr,
-      "timestamp" => $timestamp,
-      "url"       =>$url,
-      "signature" => $signature,
-      "rawString" => $string,
-    );
-    return $signPackage; 
-  }
+        $timestamp = time();
+        $nonceStr = Common::createNonceStr();
 
+        // 这里参数的顺序要按照 key 值 ASCII 码升序排序
+        $string = "jsapi_ticket=$jsapiTicket&noncestr=$nonceStr&timestamp=$timestamp&url=$url";
 
-
-  public function getJsApiTicket() {
-    // jsapi_ticket 应该全局存储与更新，以下代码以写入到文件中做示例
-    $data = json_decode($this->get_php_file("../../config/jsapi_ticket.php"));
-    if ($data->expire_time < time()) {
-      $accessToken = $this->getAccessToken();
-      // 如果是企业号用以下 URL 获取 ticket
-      $url = sprintf(WechatConfig::GET_JSAPI_TICKET_URL, $accessToken);
-      $res = json_decode(httpGet($url));
-
-      $ticket = $res->ticket;
-      if ($ticket) {
-        $data->expire_time = time() + (int)WechatConfig::EXPIRE_ACCESS_TOKEN;
-        $data->jsapi_ticket = $ticket;
-        $this->set_php_file("../../jsapi_ticket.php", json_encode($data));
-      }
-    } else {
-      $ticket = $data->jsapi_ticket;
+        $signature = sha1($string);
+        $signPackage = array(
+            "appId"     => $this->appId,
+            "nonceStr"  => $nonceStr,
+            "timestamp" => $timestamp,
+            "url"       =>$url,
+            "signature" => $signature,
+            "rawString" => $string,
+        );
+        return $signPackage;
     }
 
-    return $ticket;
-  }
 
-  public function getAccessToken() {
-    // access_token 应该全局存储与更新，以下代码以写入到文件中做示例
-    $data = json_decode($this->get_php_file("data/file/access_token.php"));
 
-    if ($data->expire_time < time()) {
+    public function getJsApiTicket() {
+        // jsapi_ticket 应该全局存储与更新，以下代码以写入到文件中做示例
+        $data = Cache::get("jsapiTicket");
+        if (!$data || $data['expire_time'] < time()) {
+            $accessToken = $this->getAccessToken();
+            // 如果是企业号用以下 URL 获取 ticket
+            $url = sprintf(WechatConfig::GET_JSAPI_TICKET_URL, $accessToken);
+            $res = json_decode(Common::postCurl($url, 'GET'), true);
 
-      // 如果是企业号用以下URL获取access_token
-      $url = sprintf(WechatConfig::GET_ACCESS_TOKEN_URL, $this->appid, $this->appSecret);
-      $res = json_decode(httpGet($url));
-      $access_token = $res->access_token;
-      if ($access_token) {
-        $data->expire_time = time() + (int)WechatConfig::EXPIRE_ACCESS_TOKEN;
-        $data->access_token = $access_token;
-        $this->set_php_file("../../access_token.php", json_encode($data));
-      }
-    } else {
-      $access_token = $data->access_token;
+            $ticket = $res['ticket'];
+            if ($ticket) {
+                $data['expire_time'] = time() + (int)WechatConfig::EXPIRE_ACCESS_TOKEN;
+                $data['jsapi_ticket'] = $ticket;
+                Cache::set("jsapiTicket",$data);
+            }
+        } else {
+            $ticket = $data['jsapi_ticket'];
+        }
+
+        return $ticket;
     }
-    return $access_token;
-  }
 
-  private function get_php_file($filename) {
-    return trim(substr(file_get_contents($filename), 15));
-  }
-  private function set_php_file($filename, $content) {
-	   file_put_contents($filename,"<?php exit();?>" . $content);
-  }
+    public function getAccessToken() {
+        // access_token 应该全局存储与更新，以下代码以写入到文件中做示例
+//    $data = json_decode($this->get_php_file("data/file/access_token.php"));
+        $data = Cache::get("accessToken");
+        if (!$data || $data['expire_time'] < time()) {
+
+            // 如果是企业号用以下URL获取access_token
+            $url = sprintf(WechatConfig::GET_ACCESS_TOKEN_URL, $this->appid, $this->appSecret);
+            $res = json_decode(Common::postCurl($url, 'GET'), true);
+            $access_token = $res['access_token'];
+            if ($access_token) {
+                $data['expire_time'] = time() + (int)WechatConfig::EXPIRE_ACCESS_TOKEN;
+                $data['access_token'] = $access_token;
+//        $this->set_php_file("../../access_token.php", json_encode($data));
+                Cache::set("accessToken",$data);
+            }
+        } else {
+            $access_token = $data->access_token;
+        }
+        return $access_token;
+    }
+
+    private function get_php_file($filename) {
+        return trim(substr(file_get_contents($filename), 15));
+    }
+    private function set_php_file($filename, $content) {
+        file_put_contents($filename,"<?php exit();?>" . $content);
+    }
 }
 
